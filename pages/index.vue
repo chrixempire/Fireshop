54d649f0a8747ceead37d71cd397460e35046e3e
chris <anyikamaduchris@gmail.com>
2023-06-24 16:34:08 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+ <div >
+  <div class="container">
+    <heroes/>
+  <largeCardDisplay
+  v-for="cardInfo in largeCardInfo"
+  :key="cardInfo.id"
+  :cardSection="cardInfo"
+  />
+  <smallCardDisplay
+    v-for="cardInfo in smallCardSections"
+    :key="cardInfo.id"
+    :cardSection="cardInfo"
+  />
+  </div>
+ </div>
+</template>
+
+<script>
+import heroes from '~/components/heroes.vue'
+import {largeCardSections, smallCardSections } from '@/assets/data.js'
+import largeCardDisplay from '~/components/largeCardDisplay.vue'
+import smallCardDisplay from '~/components/smallCardDisplay.vue'
+
+export default {
+  layout: 'appLayout',
+  components: { heroes, largeCardDisplay, smallCardDisplay
+  
+  },
+  name: 'IndexPage',
+  data() {
+    return{
+      largeCardInfo : largeCardSections,
+      smallCardSections 
+    }
+  }
+}
+</script>
